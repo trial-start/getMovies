e25be00bcf44f5be470a21e98b4a896b083e1767
trial-start <143869677+trial-start@users.noreply.github.com>
2024-04-12 21:33:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -432,9 +432,9 @@ function Movie({ movie, onSelectMovie }) {
 }
 
 function WatchedSummary({ watched }) {
-  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-  const avgUserRating = average(watched.map((movie) => movie.userRating));
-  const avgRuntime = average(watched.map((movie) => movie.runtime));
+  const avgImdbRating = average(watched?.map((movie) => movie.imdbRating));
+  const avgUserRating = average(watched?.map((movie) => movie.userRating));
+  const avgRuntime = average(watched?.map((movie) => movie.runtime));
 
   return (
     <div className="summary">
