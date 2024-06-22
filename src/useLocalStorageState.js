e1ab52e70cfb072d68417c06a86eb52089d99b5c
trial-start <143869677+trial-start@users.noreply.github.com>
2024-06-22 23:21:03 +0530

    Update useLocalStorageState.js

--- src/useLocalStorageState.js
+++ src/useLocalStorageState.js
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
 export function useLocalStorageState(initial, key) {
-  const [value, setValue] = useState(function () {
-    const getMovies = localStorage.getItem(key);
-    return JSON.parse(getMovies);
-  });
+  const [value, setValue] = useState(initial);
+  // const [value, setValue] = useState(function () {
+  //   const getMovies = localStorage.getItem(key);
+  //   return JSON.parse(getMovies);
+  // });
 
   useEffect(
     function () {
